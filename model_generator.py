1701a4037ee221b53ff51d51a856d26b02ae90c8
Nacho Castejón <nachoo@gmail.com>
2017-12-10 02:29:13 +0100

    Using left and right cameras, generator

--- model_generator.py
+++ model_generator.py
@@ -1,8 +1,11 @@
-from model_utils import load_recordings
+from model_utils import parse_recordings, image_generator
 from keras.layers import Flatten, Dense, Cropping2D, Lambda, Conv2D, BatchNormalization, Dropout, MaxPooling2D
 from keras.models import Sequential
+from sklearn.model_selection import train_test_split
 
-X_train, y_train = load_recordings('data', 'drive2', 'recenter')
+X_train, y_train = parse_recordings('data', 'drive2', 'recenter')
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
 model = Sequential()
 model.add(Cropping2D(cropping=((60, 20), (0, 0)), input_shape=(160, 320, 3)))
@@ -20,6 +23,12 @@ model.add(Dense(1))
 
 print(model.summary())
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, batch_size=64, verbose=1)
+model.fit_generator(generator=image_generator(X_train, y_train), samples_per_epoch=len(X_train),
+                    validation_data=image_generator(X_valid, y_valid),
+                    nb_val_samples=len(X_valid),
+                    nb_epoch=5, verbose=1)
 
 model.save('model.h5')
+
+
+
--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,16 @@
 import csv
-
 import cv2
 import numpy as np
+
+
+def load_image(source_path, recording_name, measurement, images, measurements):
+    filename = source_path.split('/')[-1]
+    my_path = recording_name + '/IMG/' + filename
+    image=cv2.imread(my_path)
+    images.append(image)
+    measurements.append(measurement)
+    images.append(np.fliplr(image))
+    measurements.append(-measurement)
 
 
 def load_recordings(*recording_names):
@@ -16,16 +25,78 @@ def load_recordings(*recording_names):
             for line in reader:
                 lines.append(line)
         for line in lines:
-            source_path = line[0]
-            filename = source_path.split('/')[-1]
-            my_path = recording_names[recording_number] + '/IMG/' + filename
             # read image and measurement from file
-            image = cv2.imread(my_path)
             measurement = float(line[3])
-            # store image and measurement
-            images.append(image)
-            measurements.append(measurement)
-            # store flipped version
-            images.append(np.fliplr(image))
-            measurements.append(-measurement)
+            # CENTER
+            load_image(source_path=line[0], recording_name=recording_names[recording_number],
+                       measurement=measurement,
+                       images=images, measurements=measurements)
+            # LEFT
+            load_image(source_path=line[1], recording_name=recording_names[recording_number],
+                       measurement=measurement+0.2,
+                       images=images, measurements=measurements)
+            # RIGHT
+            load_image(source_path=line[2],recording_name=recording_names[recording_number],
+                       measurement=measurement-0.2,
+                       images=images, measurements=measurements)
     return np.array(images), np.array(measurements)
+
+
+def parse_recordings(*recording_names):
+    paths = []
+    measurements = []
+    for recording_number in range(len(recording_names)):
+        with open(recording_names[recording_number]+'/driving_log.csv') as file:
+            lines = []
+            reader= csv.reader(file)
+            # Skip header
+            next(reader,None)
+            for line in reader:
+                lines.append(line)
+        for line in lines:
+            # CENTER
+            filename = line[0].split('/')[-1]
+            path = recording_names[recording_number] + '/IMG/' + filename
+            measurement = float(line[3])
+            # store path and measurement for center image
+            paths.append(path)
+            measurements.append(measurement)
+
+            # LEFT
+            filename = line[1].split('/')[-1]
+            path = recording_names[recording_number] + '/IMG/' + filename
+            # store path and measurement for left image
+            paths.append(path)
+            measurements.append(measurement+0.2)
+
+            # RIGHT
+            filename = line[2].split('/')[-1]
+            path = recording_names[recording_number] + '/IMG/' + filename
+            # store path and measurement for left image
+            paths.append(path)
+            measurements.append(measurement - 0.2)
+    return paths, measurements
+
+
+def image_generator(paths, measurements, batch_size=32):
+    nsamples = len(paths)
+    while 1:
+        indexes = np.array(range(2*nsamples))
+        np.random.shuffle(indexes)
+        batch_images = []
+        batch_measurements = []
+        for i in indexes:
+            if i >= nsamples:
+                image = cv2.imread(paths[i-nsamples])
+                image = np.fliplr(image)
+                measurement = -measurements[i-nsamples]
+            else:
+                image = cv2.imread(paths[i])
+                measurement = measurements[i]
+            batch_images.append(image)
+            batch_measurements.append(measurement)
+            if len(batch_images) == batch_size:
+                yield np.array(batch_images), np.array(batch_measurements)
+                batch_images = []
+                batch_measurements = []
+
