09cdfdbaec13f61f6477acd064447d71e16ca415
Nacho Castejón <nachoo@gmail.com>
2017-12-13 21:13:43 +0100

    Code cleanup

--- model_utils.py
+++ model_utils.py
@@ -2,46 +2,8 @@ import csv
 import cv2
 import numpy as np
 
-
-def load_image(source_path, recording_name, measurement, images, measurements):
-    filename = source_path.split('/')[-1]
-    my_path = recording_name + '/IMG/' + filename
-    image=cv2.imread(my_path)
-    images.append(image)
-    measurements.append(measurement)
-    images.append(np.fliplr(image))
-    measurements.append(-measurement)
-
-
-def load_recordings(*recording_names):
-    images = []
-    measurements = []
-    for recording_number in range(len(recording_names)):
-        with open(recording_names[recording_number]+'/driving_log.csv') as file:
-            lines = []
-            reader= csv.reader(file)
-            # Skip header
-            next(reader,None)
-            for line in reader:
-                lines.append(line)
-        for line in lines:
-            # read image and measurement from file
-            measurement = float(line[3])
-            # CENTER
-            load_image(source_path=line[0], recording_name=recording_names[recording_number],
-                       measurement=measurement,
-                       images=images, measurements=measurements)
-            # LEFT
-            load_image(source_path=line[1], recording_name=recording_names[recording_number],
-                       measurement=measurement+0.2,
-                       images=images, measurements=measurements)
-            # RIGHT
-            load_image(source_path=line[2],recording_name=recording_names[recording_number],
-                       measurement=measurement-0.2,
-                       images=images, measurements=measurements)
-    return np.array(images), np.array(measurements)
-
-
+# Parse recording logs, keeping image paths and steering angles
+# with the specified bias for left and right images
 def parse_recordings(*recording_names, side_camera_bias=0.1):
     paths = []
     measurements = []
@@ -54,27 +16,32 @@ def parse_recordings(*recording_names, side_camera_bias=0.1):
             for line in reader:
                 lines.append(line)
         for line in lines:
-            # CENTER
+            # Store image paths
             for i in range(3):
                 filename = line[i].split('/')[-1]
                 path = recording_names[recording_number] + '/IMG/' + filename
                 paths.append(path)
-
+            # Retrieve steering angle for center image
             measurement_center = float(line[3])
-
+            # Store steering angles for all three images
             measurements.append(measurement_center)
             measurements.append(measurement_center+side_camera_bias)
             measurements.append(measurement_center-side_camera_bias)
     return paths, measurements
 
+# Loads images from disk and generates batchs of the specified size
+# Images are shuffled after each epoch
 def image_generator(paths, measurements, batch_size=64):
     nsamples = len(paths)
     while 1:
+        #Generate a sequence twice the size of the image pool
         indexes = np.array(range(2*nsamples))
+        #Shuffle the sequence, this will be the order for this epoch
         np.random.shuffle(indexes)
         batch_images = []
         batch_measurements = []
         for i in indexes:
+            #Indexes over the size of the image pool will result in a flipped image
             if i >= nsamples:
                 image = cv2.imread(paths[i-nsamples])
                 image = np.fliplr(image)
@@ -84,10 +51,12 @@ def image_generator(paths, measurements, batch_size=64):
                 measurement = measurements[i]
             batch_images.append(image)
             batch_measurements.append(measurement)
+            #Yield batch
             if len(batch_images) == batch_size:
                 yield np.array(batch_images), np.array(batch_measurements)
                 batch_images = []
                 batch_measurements = []
+        #Last batch
         if len(batch_images) > 0:
             yield np.array(batch_images), np.array(batch_measurements)
 
